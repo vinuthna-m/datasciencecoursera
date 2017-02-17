200643ef04237aafe2153793f3dff55847413df1
vinuthna-m <vinuthnamandalapu@gmail.com>
2017-02-17 14:22:58 -0800

    Third commit R programming

--- RProgramming/corr.R
+++ RProgramming/corr.R
@@ -1,3 +1,21 @@
 corr <- function(directory, threshold = 0){
-  
+  x <- vector(mode = "numeric", length=0)
+  temp <- complete(directory, 1:332)
+  temp <- subset(temp, nobs>=threshold)
+  vec <- temp$id
+  for(f in vec){
+    if(f<10){
+      filename <- paste("00", f, ".csv", sep = "")
+    }
+    else if((f>=10)&&(f<100)){
+      filename <- paste("0",f,".csv",sep="")
+    }
+    else{
+      filename <- paste(f,".csv",sep = "")
+    }
+    df <- read.csv(paste(directory,filename,sep = "/"))
+    df <- na.omit(df)
+    append(x, cor(df$sulfate,df$nitrate),after = length(x))
+  }
+  x
 }
